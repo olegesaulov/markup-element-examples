05d7ae292ed1b931a442d1df1b4b50a675c99174
Oleg Esaulov <esaulov.o.k@gmail.com>
2022-02-11 18:41:13 +0300

    header_menu_v1 fix

--- src/burger-menu_v1/js/app.js
+++ src/burger-menu_v1/js/app.js
@@ -2,10 +2,17 @@ const header__burger = document.querySelector(".header__burger");
 const header__menu = document.querySelector(".header__menu");
 const body = document.querySelector("body");
 
-[header__burger, header__menu].forEach(item => {
-    item.addEventListener("click", () => {
-        header__burger.classList.toggle("active");
-        header__menu.classList.toggle("active");
-        body.classList.toggle("lock");
-    });
+const headerItemClickHandler = () => {
+    header__burger.classList.toggle("active");
+    header__menu.classList.toggle("active");
+    body.classList.toggle("lock");
+}
+
+header__burger.addEventListener("click", headerItemClickHandler);
+header__menu.addEventListener("click", (event) => {
+    if (!event.currentTarget.classList.contains('active')) {
+        return;
+    }
+
+    headerItemClickHandler();
 });
