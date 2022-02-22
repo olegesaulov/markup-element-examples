b6fb8f950db11970d483a52bb6d8d8fcda99a827
Oleg Esaulov <esaulov.o.k@gmail.com>
2022-02-22 13:02:18 +0300

    burger_v1. Fix

--- src/burger-menu_v1/js/app.js
+++ src/burger-menu_v1/js/app.js
@@ -1,3 +1,4 @@
+const linkClassName = "header__link";
 const header__burger = document.querySelector(".header__burger");
 const header__menu = document.querySelector(".header__menu");
 const body = document.querySelector("body");
@@ -11,6 +12,10 @@ const headerItemClickHandler = () => {
 header__burger.addEventListener("click", headerItemClickHandler);
 header__menu.addEventListener("click", (event) => {
     if (!event.currentTarget.classList.contains('active')) {
+        return;
+    }
+
+    if (!event.target.classList.contains(linkClassName)) {
         return;
     }
 
